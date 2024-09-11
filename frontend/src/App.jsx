a53a8124d0240ecfcc3e82a491016b05a7345736
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2024-09-12 00:03:50 +0500

    navbar developnment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,33 @@ import Doctors from './pages/Doctors'
 import Contact from './pages/Contact'
 import MyAppointment from './pages/MyAppointment'
 import Login from './pages/Login'
+import About from './pages/About'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navabar from './components/Navabar'
+import Footer from './components/Footer'
+
+
 const App = () => {
 
 
   return (
     <>
     <div className='mx-4 sm:mx-[10%]'>
-
+<Navabar />
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/doctors' element={<Doctors/>} />
+      <Route path='/doctors/:speciality' element={<Doctors/>} />
       <Route path='/contact' element={<Contact/>} />
-      <Route path='/about' element={<Login/>} />
+      <Route path='/login' element={<Login/>} />
       <Route path='/myAppointment' element={<MyAppointment/>} />
-
+      <Route path='/appointment/:docId' element={<Appointment/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/my-profile' element={<MyProfile />} />
     </Routes>
+<Footer />
+
     </div>
     </>
   )
---
+++ frontend/src/pages/Appointment.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Appointment = () => {
+
+  return (
+
+    <>
+      <div className='text-red-700 font-light'>
+        <h1>Appointment</h1>
+      </div>
+
+    </>
+
+  )
+}
+
+export default Appointment
